85ef3010f866636b023f5705a64849be165c1102
alxrgdriguez <sralexred@gmail.com>
2025-01-28 16:17:01 +0100

    Ultimo commit

--- astro_pokemon/astro.config.mjs
+++ astro_pokemon/astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  
-  redirects: {'/': '/pokemons/1'},
+  integrations: [tailwind()]
 });
