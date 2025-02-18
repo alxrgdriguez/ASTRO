6d6ba58d779869073d85211a8cb00cd9d1e5569b
alxrgdriguez <sralexred@gmail.com>
2025-02-18 18:59:03 +0100

    blog_casiTerminado

--- blog_astro/src/content/config.ts
+++ blog_astro/src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 
 const blogCollection = defineCollection({
@@ -10,14 +10,30 @@ const blogCollection = defineCollection({
         image: image(),
 
         // Relacion
-        author:z.string(), 
+        // author:z.string(), 
+        author: reference('author'),
 
         //Relacion
         tags: z.array(z.string()),
+
+        // Boolean 
+        isDraft: z.boolean().default(false),
     }),
 
 });
 
+const authorCollection = defineCollection(
+    {
+        type: "data",
+        schema: ({image}) => z.object({
+            name: z.string(),
+            avatar: image(),
+        }),
+    }
+);
+        
+
 export const collections = {
     blog: blogCollection,
+    author: authorCollection,
 };
