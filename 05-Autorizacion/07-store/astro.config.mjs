32b5fce73523071caca3b77c7df077c287356f4c
alxrgdriguez <sralexred@gmail.com>
2025-03-12 20:28:11 +0100

    Funcionado_login

--- 05-Autorizacion/07-store/astro.config.mjs
+++ 05-Autorizacion/07-store/astro.config.mjs
@@ -6,11 +6,8 @@ import netlify from '@astrojs/netlify';
 
 import auth from 'auth-astro';
 
-<<<<<<< HEAD
 import db from '@astrojs/db';
 
-=======
->>>>>>> bdaebce10f1fe17350005ca82e7ef6f6814f5439
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -18,9 +15,5 @@ export default defineConfig({
   },
 
   adapter: netlify(),
-<<<<<<< HEAD
   integrations: [auth(), db()]
-=======
-  integrations: [auth()]
->>>>>>> bdaebce10f1fe17350005ca82e7ef6f6814f5439
 });
